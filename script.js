b74956e2656f21fcee58904cc7aaf752c1907860
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-04 12:48:17 +0300

    New

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function verifyDiagonal(row, column, countYellowDisks, countRedDisks) {
         for (let i = row, j = column; exists == true && sameColor.val == true; i += count1, j += count2) {
             console.log(i + ' ' + j);
             let circle = document.getElementById('circle' + (i * 10 + j) + '');
-            if (i + 1 <= 6 && j + 1 <= 7 && counter == 1) {
+            if (i + 1 <= 6 && j + 1 <= 7 && count1 == 1 && count2 == 1) {
                 console.log('Adevarat');
                 array[step] += 1;
                 console.log(array.val[step]);
