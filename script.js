f7391939c30e8ed4b7b576e7c3937acda101fe5c
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-04 21:50:07 +0300

    New3

--- script.js
+++ script.js
@@ -73,15 +73,7 @@ function verifyDiagonal(row, column, countYellowDisks, countRedDisks) {
                 console.log('Adevarat2');
                 let neighbour = document.getElementById('circle' + ((i + count1) * 10 + (j + count2)) + '');
                 compare(circle, neighbour, yellow2, red2, sameColor);
-            }  /*else if (i - 1 >= 1 && j + 1 <= 7 && count1 == -1 && count2 == 1) {
-                console.log('Adevarat3');
-                let neighbour = document.getElementById('circle' + ((i + count1) * 10 + (j + count2)) + '');
-                compare(circle, neighbour, yellow2, red2, sameColor);
-            } else if (i - 1 >= 1 && j - 1 >= 1 && count1 == -1 && count2 == -1) {
-                console.log('Adevarat4');
-                let neighbour = document.getElementById('circle' + ((i + count1) * 10 + (j + count2)) + '');
-                compare(circle, neighbour, yellow1, red1, sameColor);
-            }*/ else {
+            } else {
                 exists = false;
             }
         }
