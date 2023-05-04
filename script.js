9ca5ff692fd8a3fc3f88b4051d804a7c2c649b99
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-04 12:59:24 +0300

    New

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function outputCondition(countYellowDisks, countRedDisks) {
 function verifyDiagonal(row, column, countYellowDisks, countRedDisks) {
     const sameColor = {val: true};
     let count1 = 1, count2 = 1;
-    const array = {val : [0, 0, 0, 0]};
+    const array = {val : [0, 0, 0, 0, 0]};
     for (let step = 1; step <= 4; ++step) {
         let exists = true;
         console.log('Pas' + step);
